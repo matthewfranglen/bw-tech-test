c940721f4b1f0f1b9281f0735e687a189913cb87
Matthew Franglen <matthew@franglen.org>
2014-10-08 23:40:53 +0100

    Initial card definition
    Really quite happy with this. Need to pass in the image to use and the
    file to download.

---
+++ web/x-card.dart
@@ -0,0 +1,13 @@
+library website.card;
+
+import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer click counter element.
+ */
+@CustomTag('x-card')
+class CardElement extends PolymerElement {
+
+  CardElement.created() : super.created();
+}
+
